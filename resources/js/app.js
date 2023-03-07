ccb590ccc408f3e2d2d46125ba80e2a39a7d48fc
tomossmith <tom@tomossmith.com>
2023-03-07 19:12:01 +0000

    updated tasks layout, added sort function

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,41 @@
-import './bootstrap';
+import "./bootstrap";
 
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
+import Alpine from "alpinejs";
+import focus from "@alpinejs/focus";
 window.Alpine = Alpine;
 
 Alpine.plugin(focus);
 
 Alpine.start();
+
+const ascsortButton = document.getElementById("asc-date-sort-button");
+const decsortButton = document.getElementById("dec-date-sort-button");
+const tableBody = document.querySelector("tbody");
+
+// Sort by ascending date
+ascsortButton.addEventListener("click", () => {
+    const rows = Array.from(tableBody.querySelectorAll("tr"));
+
+    rows.sort((a, b) => {
+        const dateA = new Date(a.querySelector("td:nth-child(2)").textContent);
+        const dateB = new Date(b.querySelector("td:nth-child(2)").textContent);
+
+        return dateA - dateB;
+    });
+
+    tableBody.append(...rows);
+});
+
+// Sort by descending date
+decsortButton.addEventListener("click", () => {
+    const rows = Array.from(tableBody.querySelectorAll("tr"));
+
+    rows.sort((a, b) => {
+        const dateA = new Date(a.querySelector("td:nth-child(2)").textContent);
+        const dateB = new Date(b.querySelector("td:nth-child(2)").textContent);
+
+        return dateB - dateA;
+    });
+
+    tableBody.append(...rows);
+});
