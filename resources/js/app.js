ada7beb911c2d66c1817adb2a7693544f15d95db
tomossmith <tom@tomossmith.com>
2023-03-09 11:08:03 +0000

    fixed an event listening error on pages other than the dashboard

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,38 +22,44 @@ Alpine.start();
 //    }
 //}
 
+// Sort task table by date order
+
+
 const ascsortButton = document.getElementById("asc-date-sort-button");
 const decsortButton = document.getElementById("dec-date-sort-button");
 const tableBody = document.querySelector("tbody");
 
 // Sort task table by ascending date
-//ascsortButton.addEventListener("click", () => {
-//    const rows = Array.from(tableBody.querySelectorAll("tr"));
-//
-//    rows.sort((a, b) => {
-//        const dateA = new Date(a.querySelector("td:nth-child(2)").textContent);
-//        const dateB = new Date(b.querySelector("td:nth-child(2)").textContent);
-//
-//        return dateA - dateB;
-//    });
-//
-//    tableBody.append(...rows);
-//});
+if (ascsortButton) {
+ascsortButton.addEventListener("click", () => {
+    const rows = Array.from(tableBody.querySelectorAll("tr"));
+
+    rows.sort((a, b) => {
+        const dateA = new Date(a.querySelector("td:nth-child(2)").textContent);
+        const dateB = new Date(b.querySelector("td:nth-child(2)").textContent);
+
+        return dateA - dateB;
+    });
+
+    tableBody.append(...rows);
+})};
 
 // Sort task table by descending date
-//decsortButton.addEventListener("click", () => {
-//    const rows = Array.from(tableBody.querySelectorAll("tr"));
-//
-//    rows.sort((a, b) => {
-//        const dateA = new Date(a.querySelector("td:nth-child(2)").textContent);
-//        const dateB = new Date(b.querySelector("td:nth-child(2)").textContent);
-//
-//        return dateB - dateA;
-//    });
-//
-//    tableBody.append(...rows);
-//});
+if (decsortButton) {
+decsortButton.addEventListener("click", () => {
+    const rows = Array.from(tableBody.querySelectorAll("tr"));
 
+    rows.sort((a, b) => {
+        const dateA = new Date(a.querySelector("td:nth-child(2)").textContent);
+        const dateB = new Date(b.querySelector("td:nth-child(2)").textContent);
+
+        return dateB - dateA;
+    });
+
+    tableBody.append(...rows);
+})};
+
+// Show/Hide due date picker
 $("#due_date_checkbox").change(function() {
     if ( $(this).is(':checked') ) {
         $("#due_date_picker").show();
